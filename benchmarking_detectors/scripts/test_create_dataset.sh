cec676ee24b9dc574cc0481579d16b3a2fba14a1
marluxiabossls <henrique.dasilvagameiro@epfl.ch>
2024-08-06 12:03:13 +0200

    added prometheus eval sumbodule

--- benchmarking_detectors/scripts/test_create_dataset.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#python create_dataset.py --dataset_size=$dataset_size --max_sample_len=$max_sample_len --prefix_size=$prefix_size --dataset_name=$dataset_name --generator_name=$generator_name --attack_name=$attack_name --watermarking_scheme_name=$watermarking_scheme_name --batch_size=$batch_size --device=$device
-#python create_dataset.py generation=gen_params_attack generation.temperature=1.0
-#python create_dataset.py generation=generation_base watermark=watermark_kgw generation.dataset_size=30 generation.experiment_name=test_new_cleaning2
-python create_dataset.py generation=generation_base watermark=watermark_kgw_p generation.dataset_size=30 generation.experiment_name=test_new_watermarking
-
-
-
