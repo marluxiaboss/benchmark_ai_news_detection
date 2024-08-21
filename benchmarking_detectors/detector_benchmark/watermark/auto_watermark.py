ea3df038d287341a77fbd434f87eac3e6b02b474
marluxiabossls <henrique.dasilvagameiro@epfl.ch>
2024-08-21 16:26:45 +0200

    added base sweet watermarking

--- benchmarking_detectors/detector_benchmark/watermark/auto_watermark.py
+++ benchmarking_detectors/detector_benchmark/watermark/auto_watermark.py
@@ -15,6 +15,7 @@ WATERMARK_MAPPING_NAMES={
     "EXP": "watermark.exp.EXP",
     "DIP": "watermark.dip.DIP",
     "KGW_E": "watermark.kgw_e.KGW_E",
+    "SWEET": "watermark.sweet.SWEET",
     "SWEET_P": "watermark.sweet_p.SWEET_P",
 }
 
