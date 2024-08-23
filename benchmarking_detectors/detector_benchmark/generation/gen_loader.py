c948d7aa548c7fcfd1a18cba4a7c7b89b9eecd29
marluxiabossls <henrique.dasilvagameiro@epfl.ch>
2024-08-23 14:43:29 +0200

    added human_eval

--- benchmarking_detectors/detector_benchmark/generation/gen_loader.py
+++ benchmarking_detectors/detector_benchmark/generation/gen_loader.py
@@ -44,6 +44,34 @@ class GenLoader:
         # load generator
         match model_name:
             
+            case "qwen2_0_5B":
+                gen_path = "Qwen/Qwen2-0.5B"
+                gen_tokenizer = AutoTokenizer.from_pretrained(
+                    gen_path,
+                    pad_token='<|extra_0|>',
+                    eos_token='<|endoftext|>',
+                    padding_side='left',
+                    trust_remote_code=True
+                )
+                if self.gen_tokenizer_only:
+                    gen = None
+                else:
+                    gen = AutoModelForCausalLM.from_pretrained(
+                        gen_path,
+                        torch_dtype="auto",
+                        device_map="auto",
+                        pad_token_id=gen_tokenizer.pad_token_id,
+                    )
+
+                # config for chat template and gen parameters
+                use_chat_template = False
+                chat_template_type = None
+                gen_config = ModelConfig(gen_tokenizer,
+                    use_chat_template=use_chat_template, chat_template_type=chat_template_type,
+                    gen_params=default_gen_params, model_name=model_name, device=device)
+
+                gen_model = LLMGenerator(gen, gen_config)
+            
             case "qwen2_chat_0_5B":
                 gen_path = "Qwen/Qwen2-0.5B-Instruct"
                 gen_tokenizer = AutoTokenizer.from_pretrained(
@@ -171,6 +199,37 @@ class GenLoader:
                     gen_params=gen_params, model_name=model_name, device=device)
 
                 gen_model = LLMGenerator(gen, gen_config)
+            
+            case "llama3_3.1":
+                gen_path = "meta-llama/Meta-Llama-3.1-8B"
+                gen_tokenizer = AutoTokenizer.from_pretrained(gen_path, trust_remote_code=True)
+                gen_tokenizer.pad_token = '<|eot_id|>'
+                gen_tokenizer.padding_side = "left"
+
+                if self.gen_tokenizer_only:
+                    gen = None
+                else:
+                    gen = AutoModelForCausalLM.from_pretrained(gen_path, torch_dtype=torch.bfloat16, device_map="auto")
+
+                # config for chat template and gen parameters
+                use_chat_template = True
+                chat_template_type = "system_user"
+                
+                # special for llama3
+                terminators = [
+                    gen_tokenizer.eos_token_id,
+                    gen_tokenizer.convert_tokens_to_ids("<|eot_id|>")
+                ]
+                gen_params = default_gen_params     
+                gen_params["eos_token_id"] = terminators
+                
+                gen_config = ModelConfig(gen_tokenizer,
+                    use_chat_template=use_chat_template, chat_template_type=chat_template_type,
+                    gen_params=gen_params, model_name=model_name, device=device)
+
+                gen_model = LLMGenerator(gen, gen_config)
+                
+            
             case _:
                 # no other generator is supported for now
                 raise ValueError(f"Generator {model_name} not supported yet")
