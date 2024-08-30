ee76d6c3d5c0f1e93cc9b9d4a8eed7eb7879910e
marluxiabossls <henrique.dasilvagameiro@epfl.ch>
2024-08-30 10:41:04 +0200

    updated readme and gitignore

--- benchmarking_detectors/bash_scripts/bigcode_eval/evaluation/big_code_eval_sweet_p.sh
+++ benchmarking_detectors/bash_scripts/bigcode_eval/evaluation/big_code_eval_sweet_p.sh
@@ -4,23 +4,25 @@ task_names="humanevalplus,mbppplus"
 watermarking_scheme="SWEET_P"
 
 task="humanevalplus"
-generation_file="generations/generations_${watermarking_scheme}_${task}.json"
+generation_file="generations/gen_n_samples_50/generations_${watermarking_scheme}_${task}.json"
 metric_output_path="evaluation_results/${watermarking_scheme}_${task}_results.json"
 
 accelerate launch  main.py \
     --tasks $task \
     --allow_code_execution \
     --load_generations_path $generation_file \
-    --metric_output_path $metric_output_path
+    --metric_output_path $metric_output_path \
+    --n_samples 50
     #--model incoder-temperature-08
 
 task="mbppplus"
-generation_file="generations/generations_${watermarking_scheme}_${task}.json"
+generation_file="generations/gen_n_samples_50/generations_${watermarking_scheme}_${task}.json"
 metric_output_path="evaluation_results/${watermarking_scheme}_${task}_results.json"
 
 accelerate launch  main.py \
     --tasks $task \
     --allow_code_execution \
     --load_generations_path $generation_file \
-    --metric_output_path $metric_output_path
+    --metric_output_path $metric_output_path \
+    --n_samples 50
     #--model incoder-temperature-08
