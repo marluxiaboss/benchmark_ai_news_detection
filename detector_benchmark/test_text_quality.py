eaf9b38e7400cb46e30492d0b7e20feef2698419
marluxiaboss <henrique.dasilvagameiro@epfl.ch>
2024-09-20 09:39:31 +0200

    updated readme and typo

--- detector_benchmark/test_text_quality.py
+++ detector_benchmark/test_text_quality.py
@@ -1,8 +1,15 @@
 import argparse
-from text_quality_evaluation import (Scorer, RefScorer, BertScoreScorer,
-    SemScoreScorer, IDFScorer, PrometheusScorer, PPLScorer)
-from generation import GenLoader
-from pipeline import TextQualityPipeline
+from .text_quality_evaluation import (
+    Scorer,
+    RefScorer,
+    BertScoreScorer,
+    SemScoreScorer,
+    IDFScorer,
+    PrometheusScorer,
+    PPLScorer,
+)
+from .generation import GenLoader
+from .pipeline import TextQualityPipeline
 import numpy as np
 from datasets import load_dataset
 import json
@@ -12,57 +19,72 @@ import os
 import hydra
 from omegaconf import DictConfig, OmegaConf
 
-from utils.utils import create_logger_file
+from .utils.utils import create_logger_file
+
 
 def init_pipelines(cfg: DictConfig, log):
-    
+
     # print the pipeline config
     for key in cfg.pipeline:
         log.info(f"{key}: {cfg.pipeline[key]}")
-    
+
     dataset_name = cfg.pipeline.dataset_name
     data_experiment_name_main = cfg.pipeline.data_experiment_name_main
     data_experiment_name_compare = cfg.pipeline.data_experiment_name_compare
     generator_name = cfg.pipeline.generator_name
     watermarking_scheme_name_main = cfg.pipeline.watermarking_scheme_name_main
-    
+
     # flag to decide whether we evaluate human text as the main text
     eval_human = cfg.pipeline.eval_human
-    
+
     # can be set to another watermarking scheme (or no watermarking scheme) if we want to compare different watermarking scheme against each other
     watermarking_scheme_name_compare = cfg.pipeline.watermarking_scheme_name_compare
 
     watermarked_dataset_path_main = f"data/generated_datasets/{dataset_name}/no_attack/{watermarking_scheme_name_main}/{generator_name}_{data_experiment_name_main}"
     watermarked_dataset_path_compare = f"data/generated_datasets/{dataset_name}/no_attack/{watermarking_scheme_name_compare}/{generator_name}_{data_experiment_name_compare}"
-    
+
     pipelines = []
-    
+
     if cfg.pipeline.use_bert_scorer:
         bert_scorer = BertScoreScorer("bert_score")
-        pipeline = TextQualityPipeline(bert_scorer, watermarked_dataset_path_main,
-            dataset_path_compare=watermarked_dataset_path_compare, batch_size=cfg.pipeline.batch_size, return_loss_lists=cfg.pipeline.return_loss_lists)
+        pipeline = TextQualityPipeline(
+            bert_scorer,
+            watermarked_dataset_path_main,
+            dataset_path_compare=watermarked_dataset_path_compare,
+            batch_size=cfg.pipeline.batch_size,
+            return_loss_lists=cfg.pipeline.return_loss_lists,
+        )
         pipelines.append(pipeline)
-        
+
     if cfg.pipeline.use_idf_scorer:
         cnn_dailymail = load_dataset("cnn_dailymail", "3.0.0")["train"]
         cnn_dailymail = cnn_dailymail.shuffle(seed=42)
         idf_scorer = IDFScorer("idf_score", cnn_dailymail["article"][:10000])
-        pipeline = TextQualityPipeline(idf_scorer, watermarked_dataset_path_main, batch_size=cfg.pipeline.batch_size)
+        pipeline = TextQualityPipeline(
+            idf_scorer, watermarked_dataset_path_main, batch_size=cfg.pipeline.batch_size
+        )
         pipelines.append(pipeline)
-        
+
     if cfg.pipeline.use_prometheus_scorer:
-        
-        assert watermarking_scheme_name_main != watermarking_scheme_name_compare, "Need to provide two different watermarking schemes"
-        
+
+        assert (
+            watermarking_scheme_name_main != watermarking_scheme_name_compare
+        ), "Need to provide two different watermarking schemes"
+
         # flag to decide whether the comparison is between the watermarking scheme or with human to ai
         compare_ai_to_human = cfg.pipeline.compare_ai_to_human
-        
+
         prometheus_scorer = PrometheusScorer("prometheus_score", compare_ai_to_human)
-        pipeline = TextQualityPipeline(prometheus_scorer, watermarked_dataset_path_main, watermarked_dataset_path_compare, batch_size=cfg.pipeline.batch_size)
+        pipeline = TextQualityPipeline(
+            prometheus_scorer,
+            watermarked_dataset_path_main,
+            watermarked_dataset_path_compare,
+            batch_size=cfg.pipeline.batch_size,
+        )
         pipelines.append(pipeline)
-        
+
     if cfg.pipeline.use_ppl_scorer:
-        
+
         # not really relevant for perplexity, could be removed
         default_gen_params = {
             "max_new_tokens": 220,
@@ -71,40 +93,48 @@ def init_pipelines(cfg: DictConfig, log):
             "top_p": 0.95,
             "repetition_penalty": 1,
             "do_sample": True,
-            "top_k": 50
+            "top_k": 50,
         }
-        
-        #gen_name = cfg.pipeline.generator_name
+
+        # gen_name = cfg.pipeline.generator_name
         ppl_scorer_name = cfg.pipeline.ppl_scorer_name
         gen_params = default_gen_params
-        device = "cuda" 
+        device = "cuda"
         # load the generator and tokenizer similarl to the create_dataset.py
         gen_loader = GenLoader(ppl_scorer_name, gen_params, device)
         gen, _, gen_config = gen_loader.load()
         ppl_scorer = PPLScorer("ppl_score", gen, gen_config.tokenizer)
-        
-        pipeline = TextQualityPipeline(ppl_scorer, watermarked_dataset_path_main, batch_size=cfg.pipeline.batch_size, 
-                                       return_loss_lists=cfg.pipeline.return_loss_lists, eval_human=eval_human)
+
+        pipeline = TextQualityPipeline(
+            ppl_scorer,
+            watermarked_dataset_path_main,
+            batch_size=cfg.pipeline.batch_size,
+            return_loss_lists=cfg.pipeline.return_loss_lists,
+            eval_human=eval_human,
+        )
         pipelines.append(pipeline)
-        
+
     return pipelines
+
 
 @hydra.main(version_base=None, config_path="conf", config_name="main")
 def evaluate_text_quality(cfg: DictConfig):
-    
+
     # setup logger
-    experiment_path = (f"{cfg.pipeline.save_res_dir}/{cfg.pipeline.watermarking_scheme_name_main}_vs_{cfg.pipeline.watermarking_scheme_name_compare}/"
-                f"{cfg.pipeline.dataset_name}/{cfg.pipeline.generator_name}/quality_test_{cfg.pipeline.data_experiment_name_main}_vs_{cfg.pipeline.data_experiment_name_compare}")
+    experiment_path = (
+        f"{cfg.pipeline.save_res_dir}/{cfg.pipeline.watermarking_scheme_name_main}_vs_{cfg.pipeline.watermarking_scheme_name_compare}/"
+        f"{cfg.pipeline.dataset_name}/{cfg.pipeline.generator_name}/quality_test_{cfg.pipeline.data_experiment_name_main}_vs_{cfg.pipeline.data_experiment_name_compare}"
+    )
 
     if not os.path.exists(experiment_path):
         os.makedirs(experiment_path)
-    
+
     log = create_logger_file(f"{experiment_path}/log.txt")
-    
+
     pipelines = init_pipelines(cfg, log)
-    
+
     assert len(pipelines) > 0, "At least one pipeline must be provided"
-    
+
     results_dict = {}
     for pipeline in pipelines:
         scorer_name = pipeline.scorer.name
@@ -114,37 +144,45 @@ def evaluate_text_quality(cfg: DictConfig):
         else:
             score, lower_bound, upper_bound = pipeline.run_pipeline()
         log.info(f"Score: {score} +/- {upper_bound - score}")
-        
+
         # transform numbers into str for the json file
         score = str(score)
         lower_bound = str(lower_bound)
         upper_bound = str(upper_bound)
-        
+
         # Save the results
         if cfg.pipeline.return_loss_lists and pipeline.scorer.name == "ppl_score":
-            results_dict[scorer_name] = {"score": score, "lower_bound": lower_bound, "upper_bound": upper_bound, "loss_lists": loss_lists}
+            results_dict[scorer_name] = {
+                "score": score,
+                "lower_bound": lower_bound,
+                "upper_bound": upper_bound,
+                "loss_lists": loss_lists,
+            }
         else:
-            results_dict[scorer_name] = {"score": score, "lower_bound": lower_bound, "upper_bound": upper_bound}
-        
+            results_dict[scorer_name] = {
+                "score": score,
+                "lower_bound": lower_bound,
+                "upper_bound": upper_bound,
+            }
+
     # save config to results dict
     results_dict["config"] = dict(cfg.pipeline)
-    
+
     # save the results to a json file
     if cfg.eval_human:
-        json_path = (f"{cfg.pipeline.save_res_dir}/human_vs_{cfg.pipeline.watermarking_scheme_name_compare}/"
-                f"{cfg.pipeline.dataset_name}/{cfg.pipeline.generator_name}/quality_test_human_vs_{cfg.pipeline.data_experiment_name_compare}.json")
-    json_path = (f"{cfg.pipeline.save_res_dir}/{cfg.pipeline.watermarking_scheme_name_main}_vs_{cfg.pipeline.watermarking_scheme_name_compare}/"
-                f"{cfg.pipeline.dataset_name}/{cfg.pipeline.generator_name}/quality_test_{cfg.pipeline.data_experiment_name_main}_vs_{cfg.pipeline.data_experiment_name_compare}.json")
-    
+        json_path = (
+            f"{cfg.pipeline.save_res_dir}/human_vs_{cfg.pipeline.watermarking_scheme_name_compare}/"
+            f"{cfg.pipeline.dataset_name}/{cfg.pipeline.generator_name}/quality_test_human_vs_{cfg.pipeline.data_experiment_name_compare}.json"
+        )
+    json_path = (
+        f"{cfg.pipeline.save_res_dir}/{cfg.pipeline.watermarking_scheme_name_main}_vs_{cfg.pipeline.watermarking_scheme_name_compare}/"
+        f"{cfg.pipeline.dataset_name}/{cfg.pipeline.generator_name}/quality_test_{cfg.pipeline.data_experiment_name_main}_vs_{cfg.pipeline.data_experiment_name_compare}.json"
+    )
+
     with open(json_path, "w") as f:
         f.write(json.dumps(results_dict, indent=4))
-    
+
 
 if __name__ == "__main__":
-    
+
     evaluate_text_quality()
-    
-    
-    
-    
-    
